b60546d580530c985c2547d39776397cb5efaef5
Uğur <ugurtosun50@gmail.com>
2022-07-30 14:22:25 +0300

    Update city

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <div className='drop-shadow-md'>
             <input onChange={(e) => setSearch(e.target.value)} 
             type="text" placeholder="Enter your city" className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-96 mt-8"/>
-            <City city={city}/>
+            {city !== undefined ? <City city={city}/> : null}
           </div>
       </div>
       
