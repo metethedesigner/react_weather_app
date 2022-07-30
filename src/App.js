b3aa7b380b6906107d8e9d918b41fa2231374a4e
Uğur <ugurtosun50@gmail.com>
2022-07-30 13:57:58 +0300

    Errors fixed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
     async function getWeather() {
       try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
-        console.log(response);
         setCity(response.data);
       } catch (error) {
         console.error(error);
@@ -19,13 +18,13 @@ function App() {
     }
     getWeather();
   },[search])
-  console.log(search)
+
   return (
     <div className="App">
       <div className='bg-slate-300 w-screen h-screen'>
           <div className='drop-shadow-md'>
             <input onChange={(e) => setSearch(e.target.value)} 
-            type="text" placeholder="Enter your city" class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-96 mt-8"/>
+            type="text" placeholder="Enter your city" className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-96 mt-8"/>
             <City city={city}/>
           </div>
       </div>
---
+++ src/components/City.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const City = ({city}) => {
+    return (
+    <div className='w-96 flex-col border-2 mt-4 mb-0 ml-auto mr-auto rounded bg-stone-200'>
+        <h1 className="mt-3 font-bold font text-3xl">{city !== undefined ? city.main.temp : ''}</h1>
+        <h1 className="mt-3 font-medium font text-2xl">{city !== undefined ?city.name : ''}</h1>
+        <h1 className="mt-3 mb-3 font text-xl">{city !== undefined ? city.weather[0].main : ''}</h1>
+    </div>
+  )
+}
+
+export default City;
