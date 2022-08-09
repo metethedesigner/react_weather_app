50a6f7d79e60a4c621561147dac0f2de501fa992
Mustafa METE <mustafa_mete1999@hotmail.com>
2022-08-09 15:20:11 +0300

    Update City.js

--- src/components/City.js
+++ src/components/City.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const City = ({city}) => {
     return (
-    <div className='w-96 flex-col border-2 mt-4 mb-0 ml-auto mr-auto rounded bg-stone-200'>
-        <h1 className="mt-3 font-bold font text-3xl">{city.main.temp}</h1>
-        <h1 className="mt-3 font-medium font text-2xl">{city.name}</h1>
-        <h1 className="mt-3 mb-3 font text-xl">{city.weather[0].main}</h1>
+    <div className='container mx-auto'>
+      <div className='w-[400px] h-[300px] pt-10 flex-col border-2 ml-auto mr-auto rounded-lg bg-stone-500 bg-opacity-20 text-white hover:border-yellow-200'>
+          <div className='flex flex-col'>
+            <h3 className='text-sm text-yellow-200'>Sıcaklık</h3>
+              <h1 className="mb-8 font-bold font text-4xl">{city.main.temp}°</h1>
+            <h3 className='text-sm text-yellow-200'>Şehir</h3>
+              <h1 className="mb-8 font-medium font text-2xl">{city.name}</h1>
+            <h3 className='text-sm text-yellow-200'>Durum</h3>
+              <h1 className="font text-xl">{city.weather[0].main}</h1>
+          </div>
+      </div>
     </div>
   )
 }
