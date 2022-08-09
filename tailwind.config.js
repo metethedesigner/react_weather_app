5a79157a813cacce04bfb002d3742cc133900d6c
Mustafa METE <mustafa_mete1999@hotmail.com>
2022-08-09 15:21:56 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    backgroundImage: theme => ({
+        'app-background': "url('img/alena-aenami-escape.jpg')"
+      }),
+    },
   },
   plugins: [],
 }
