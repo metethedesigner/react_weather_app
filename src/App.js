72392a7d64f2589abbb98b38b668bf47e5c6cf64
Mustafa METE <mustafa_mete1999@hotmail.com>
2022-08-09 15:21:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
   },[search])
 
   return (
-    <div className="App">
-      <div className='bg-slate-300 w-screen h-screen'>
+   <div className="App bg-app-background">
+      <div className='w-screen h-screen container mx-auto'>
           <div className='drop-shadow-md'>
             <input onChange={(e) => setSearch(e.target.value)} 
-            type="text" placeholder="Enter your city" className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-96 mt-8"/>
+            type="text" placeholder="Şehrinizi giriniz" className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-96 mt-10 mb-8"/>
             {city !== undefined ? <City city={city}/> : null}
           </div>
       </div>
